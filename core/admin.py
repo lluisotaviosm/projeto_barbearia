e67befcb3fac50ce49e3b5a9b3ab013ed1915879
luisdoscod <luisotavio70p@gmail.com>
2026-03-13 16:16:23 -0300

    Correções de bugs e configuração de banco de dados

--- core/admin.py
+++ core/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from .models import Barbeiro, Servico, Agendamento
 
 @admin.register(Barbeiro)
-class BarbeiroAdmin(admin.register(Barbeiro)):
+class BarbeiroAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Servico)
-class ServicoAdmin(admin.register(Servico)):
+class ServicoAdmin(admin.ModelAdmin):
     list_display = ['nome', 'preco', 'duracao_minutos']
 
 @admin.register(Agendamento)
-class AgendamentoAdmin(admin.register(Agendamento)):
+class AgendamentoAdmin(admin.ModelAdmin):
     list_display = ['cliente', 'barbeiro', 'servico', 'data', 'horario', 'confirmado']
     list_filter = ['data', 'barbeiro', 'confirmado']
