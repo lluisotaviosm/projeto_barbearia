dfad59c2be7a7a4c9f535fb96f243405aa31ff20
luisdoscod <luisotavio70p@gmail.com>
2026-03-16 19:23:39 -0300

    fix: correção de imports faltantes no views.py (BarbeiroForm)

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Barbeiro, Servico, Agendamento
+from .forms import BarbeiroForm
 from django.contrib import messages
 from django.db.models import Sum, Count
 from django.utils import timezone
